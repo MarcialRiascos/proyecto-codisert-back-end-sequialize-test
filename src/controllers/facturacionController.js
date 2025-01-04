854a8a254dc28a8aaf679c961cfc1f7881db18ab
MarcialRiascos <mecomunico2020@gmail.com>
2025-01-03 22:49:32 -0500

    Prueba nueva ruta corregida

--- src/controllers/facturacionController.js
+++ src/controllers/facturacionController.js
@@ -36,32 +36,14 @@ const facturacionController = {
     try {
       // Obtener todos los documentos de la base de datos
       const documents = await Facturacion.findAll();
-  
+
       if (documents.length === 0) {
         return res.status(404).json({ message: 'No se encontraron documentos.' });
       }
-  
-      // Construir la URL completa de cada documento
-      const baseUrl = process.env.API_URL || 'https://proyecto-codisert-back-end-sequialize.onrender.com'; // URL base de tu servidor
-  
-      // Asumiendo que cada documento tiene una propiedad 'Url' que contiene el nombre o la ruta del archivo
-      const documentsWithUrls = documents.map(document => {
-        const fileUrl = document.Url;
-  
-        // Si el campo 'Url' ya contiene una URL completa (verificar si empieza con 'http://'), usamos la URL tal cual
-        const finalUrl = fileUrl.startsWith('http://') || fileUrl.startsWith('https://')
-          ? fileUrl  // Si ya es una URL completa, no hacer nada
-          : `${baseUrl}/${fileUrl}`;  // Si es una ruta relativa, agregamos la base URL
-  
-        return {
-          ...document.dataValues, // Incluye todos los datos del documento
-          fileUrl: finalUrl, // Agrega la URL final del archivo
-        };
-      });
-  
+
       res.status(200).json({
         message: 'Documentos obtenidos correctamente.',
-        documentos: documentsWithUrls,
+        documentos: documents,
       });
     } catch (error) {
       console.error(error);
@@ -109,7 +91,7 @@ const facturacionController = {
       console.log('Facturación encontrada:', facturacion);
   
      // Extraer la ruta relativa del archivo desde Url
-     const relativePath = facturacion.Url.replace(/^https?:\/\/(?:localhost:\d+|[a-zA-Z0-9-]+\.onrender\.com|src\.[a-zA-Z0-9-]+\.[a-zA-Z]{2,6})\//, '');
+     const relativePath = facturacion.Url.replace(/^http:\/\/localhost:\d+\//, ''); // Elimina "http://localhost:3000/" o similar
      const filePath = path.resolve(__dirname, '../..', relativePath); // Construye la ruta absoluta
  
   
