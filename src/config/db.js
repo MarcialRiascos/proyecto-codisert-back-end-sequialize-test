92f83d5ca4da57d8cb1faa57fef53c909df708b1
MarcialRiascos <mecomunico2020@gmail.com>
2025-01-06 19:09:08 -0500

    esta si

--- src/config/db.js
+++ src/config/db.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'mysql',
+    dialectModule: require('mysql2'),
     logging: false,
     timezone: '-05:00', // Configura la zona horaria a UTC-5
     dialectOptions: {
