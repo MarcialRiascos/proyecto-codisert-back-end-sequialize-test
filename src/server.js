620ce855c9b9307f55d2d18a00564ee4de38780b
MarcialRiascos <mecomunico2020@gmail.com>
2025-01-07 08:36:27 -0500

    esta si

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: 'https://front-codi-test.vercel.app', // URL de tu frontend
+  origin: ['https://proyecto-codisert-frontend.vercel.app'], // URL de tu frontend
   credentials: true, // Permitir cookies y credenciales
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
   allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
